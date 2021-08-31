3f21c8d905d99a8beb0c560092f074107453dd7c
AlexGstudios <69988168+AlexGstudios@users.noreply.github.com>
2021-08-31 20:18:34 +0200

    0.1 setup for react frontend

---
+++ reactFrontend/mernfrontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';66
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Route></Route>
+    </Router>
+  );
+}
+
+export default App;
---
+++ reactFrontend/mernfrontend/src/components/fixed/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+
+export const Navbar = () => {
+
+    const logout = () => {
+
+    }
+    
+    const authNavbar = () => {
+        
+    }
+
+    const unAuthNavbar = () => {
+        
+    }
+
+    return(
+        <div className="Navbar">
+            {/* {isAuth ? authNavbar() : unAuthNavbar()} */}
+        </div>
+    )
+}

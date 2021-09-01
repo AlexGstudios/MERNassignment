ddcd00e21c31cb34d15e0bdc4aa327cb1e683089
AlexGstudios <69988168+AlexGstudios@users.noreply.github.com>
2021-09-01 21:56:58 +0200

    basic outcast

--- reactFrontend/mernfrontend/src/App.js
+++ reactFrontend/mernfrontend/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';66
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Navbar } from './components/fixed/NavBar';
+import { Home } from './components/views/Home';
+import { Login } from './components/views/Login';
+import { Register } from './components/views/Register';
+import { Map } from './components/views/Map';
+import { Account } from './components/views/Account'
+import { Footer } from './components/fixed/Footer';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <Route></Route>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/map" component={Map} />
+      <Route exact path="/account" component={Account} />
+      <Footer />
     </Router>
   );
 }
--- reactFrontend/mernfrontend/src/components/fixed/NavBar.jsx
+++ reactFrontend/mernfrontend/src/components/fixed/NavBar.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 
 export const Navbar = () => {
+
+    const [isAuth, setIsAuth] = useState(false);
 
     const logout = () => {
 
     }
     
     const authNavbar = () => {
-        
+        return(
+            <>
+                <NavLink to="/map">Map</NavLink>
+                <NavLink to="/account">Account</NavLink>
+                <NavLink to="/" onClick={logout()}>Logout</NavLink>
+                <button onClick={() => setIsAuth(false)}>AUTH</button>
+            </>
+        )
     }
 
     const unAuthNavbar = () => {
-        
+        return(
+            <>
+                <NavLink to="/login">Login</NavLink>
+                <NavLink to="/register">Register</NavLink>
+                {/* <button onClick={() => setIsAuth(true)}>UNAUTH</button> */}
+            </>
+        )
     }
 
     return(
-        <div className="Navbar">
-            {/* {isAuth ? authNavbar() : unAuthNavbar()} */}
+        <div className="navbar">
+            {isAuth ? authNavbar() : unAuthNavbar()}
         </div>
     )
 }

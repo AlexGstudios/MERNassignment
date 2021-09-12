d541cff4740f9941d4322b89854e841fe44f18cf
AlexGstudios <69988168+AlexGstudios@users.noreply.github.com>
2021-09-12 14:06:21 +0200

    frontend redone save edit and del

--- mernfrontend/src/components/subcomponents/Bottles.jsx
+++ mernfrontend/src/components/subcomponents/Bottles.jsx
@@ -1,41 +1,24 @@
 import React, {useState} from "react";
 import AuthService from "../../services/AuthService";
+import { Modal } from "../modal/NewBottle";
 
-export const Bottle = ({props}) => {
+export const Bottle = ({props, reload, deleteBottle}) => {
 
-    const [newData, setNewData] = useState();
-
-    const editBottle = async (e) => {
-        e.preventDefault();
-        const data = await AuthService(`bottles?id=${props._id}`, "put", newData);
-    }
-
-    const deleteBottle = async () => {
-        const data = await AuthService(`bottles?id=${props._id}`, "delete")
-    }
-
-    const changeData = (e) => {
-        setNewData({...props, [e.target.name]: e.target.value});
-    }
+    const [open, setOpen] = useState(false);
 
     return(
         <div className="bottle">
-            <form onSubmit={editBottle}>
-                <p>Name:</p>
-                <input type="text" name="name" onChange={changeData} placeholder={props.name}></input>
-                <p>Size:</p>
-                <input type="text" name="size" onChange={changeData} placeholder={props.size}></input>
-                <p>Price:</p>
-                <input type="text" name="price" onChange={changeData} placeholder={props.price}></input>
-                <p>Destillery:</p>
-                <input type="text" name="destillery" onChange={changeData} placeholder={props.destillery}></input>
-                <p>Rating:</p>
-                <input type="text" name="rating" onChange={changeData} placeholder={props.rating}></input>
-                <p>Region:</p>
-                <input type="text" name="region" onChange={changeData} placeholder={props.region}></input>
-                <button type="submit">Edit</button>
-            </form>
-            <button onClick={() => deleteBottle()}>Delete</button>
+            <div className={props.name}  onClick={() => setOpen(true)}>
+                <p>Name: {props.name}</p>
+                <p>Size: {props.size}</p>
+                <p>Price: {props.price}</p>
+                <p>Destillery: {props.destillery}</p>
+                <p>Rating: {props.rating}</p>
+                <p>Region: {props.region}</p>
+            </div>
+            <button onClick={() => setOpen(true)}>Edit</button>
+            <Modal props={props} open={open} onClose={() => setOpen(false)} reload={reload} title="Edit Bottle" />
+            <button onClick={() => deleteBottle(props._id)} >Delete</button>
         </div>
     )
 }

59b523e102e7e61c5f62ce8dc0f6d7a7df0034fe
azeam <dennishagg@gmail.com>
2021-09-13 08:58:03 +0200

    suggested adjustments for async reload

--- mernfrontend/src/components/modal/NewBottle.js
+++ mernfrontend/src/components/modal/NewBottle.js
@@ -1,67 +1,86 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import AuthService from "../../services/AuthService";
 
-export const Modal = ({props, open, onClose, title, reload, standardInfo}) => {
+export const Modal = ({ props, open, onClose, title, reload }) => {
+  const [info, setInfo] = useState();
 
-    const _standardInfo = {
-        name: "Name",
-        size: "Size",
-        price: "Price",
-        destillery: "Destillery",
-        rating: "Rating",
-        region: "Region"
+  const changeInfo = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+
+  const saveEdit = async (e) => {
+    e.preventDefault();
+    const data =
+      title === "New Bottle"
+        ? await AuthService("newbottle", "post", info)
+        : await AuthService(`bottles?id=${props._id}`, "put", info);
+    if (data) {
+      reload();
     }
+  };
 
-    const [info, setInfo] = useState(_standardInfo);
+  if (!open) {
+    return null;
+  }
 
-    useEffect(() => {
-        if(title === "Edit Bottle"){
-            setInfo(props);
-        }
-        if(title === "New Bottle"){
-            setInfo(_standardInfo)
-        }
-    }, [])
-
-    const changeInfo = (e) => {
-        setInfo({...info, [e.target.name]: e.target.value});
-    }
-
-    const saveEdit = async (e) => {
-        e.preventDefault();
-        if(title === "New Bottle"){
-            const data = await AuthService("newbottle", "post", info);
-            setInfo(_standardInfo)
-        }
-        if(title === "Edit Bottle"){
-            const data = await AuthService(`bottles?id=${props._id}`, "put", info);
-        }
-    }
-
-    if(!open){
-        return null;
-    }
-
-    return ReactDOM.createPortal(
+  return ReactDOM.createPortal(
+    <div>
+      <div className="overlay" onClick={onClose}></div>
+      <div className="Modal">
+        <h2>{title}</h2>
+        <button className="newbottleback" onClick={onClose}>
+          Back
+        </button>
         <div>
-            <div className="overlay" onClick={onClose}></div>
-            <div className="Modal">
-                <h2>{title}</h2>
-                <button className="newbottleback" onClick={onClose}>Back</button>
-                <div>
-                    <form onSubmit={saveEdit}>
-                        <input type="name" name="name" onChange={changeInfo} placeholder={info.name} />
-                        <input type="size" name="size" onChange={changeInfo} placeholder={info.size} />
-                        <input type="price" name="price" onChange={changeInfo} placeholder={info.price} />
-                        <input type="destillery" name="destillery" onChange={changeInfo} placeholder={info.destillery} />
-                        <input type="rating" name="rating" onChange={changeInfo} placeholder={info.rating} />
-                        <input type="region" name="region" onChange={changeInfo} placeholder={info.region} />
-                        <button type="submit" onClick={reload} >Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>,
-        document.getElementById('portal')
-    )
-}
+          <form onSubmit={saveEdit}>
+            <input
+              type="name"
+              name="name"
+              onChange={changeInfo}
+              placeholder="Name"
+              defaultValue={props ? props.name : ""}
+            />
+            <input
+              type="size"
+              name="size"
+              onChange={changeInfo}
+              placeholder="Size"
+              defaultValue={props ? props.size : ""}
+            />
+            <input
+              type="price"
+              name="price"
+              onChange={changeInfo}
+              placeholder="Price"
+              defaultValue={props ? props.prize : ""}
+            />
+            <input
+              type="destillery"
+              name="destillery"
+              onChange={changeInfo}
+              placeholder="Destillery"
+              defaultValue={props ? props.destillery : ""}
+            />
+            <input
+              type="rating"
+              name="rating"
+              onChange={changeInfo}
+              placeholder="Rating"
+              defaultValue={props ? props.rating : ""}
+            />
+            <input
+              type="region"
+              name="region"
+              onChange={changeInfo}
+              placeholder="Region"
+              defaultValue={props ? props.region : ""}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>,
+    document.getElementById("portal")
+  );
+};
--- mernfrontend/src/components/views/Whisky.jsx
+++ mernfrontend/src/components/views/Whisky.jsx
@@ -1,48 +1,70 @@
-import React, { useEffect, useState, useContext } from 'react';
-import AuthService from '../../services/AuthService';
-import { AuthContext } from '../../context/AuthProvider';
-import {Bottle} from '../subcomponents/Bottles';
-import { Modal } from '../modal/NewBottle';
+import React, { useEffect, useState, useContext } from "react";
+import AuthService from "../../services/AuthService";
+import { AuthContext } from "../../context/AuthProvider";
+import { Bottle } from "../subcomponents/Bottles";
+import { Modal } from "../modal/NewBottle";
 
 export const Whisky = () => {
+  const { authenticated } = useContext(AuthContext);
+  const [userBottles, setUserBottles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [reLoad, setReLoad] = useState(true);
 
-
-    const { authenticated} = useContext(AuthContext);
-    const [userBottles, setUserBottles] = useState([]);
-    const [isTrue, setIsTrue] = useState(false);
-    const [reLoad, setReLoad] = useState(false);
-
-    const getBottle = async () => {
-        const data = await AuthService("getbottles");
-        setUserBottles(data.message.msgBody);
-        setReLoad(false)
+  const getBottle = async () => {
+    const data = await AuthService("getbottles");
+    if (data) {
+      setUserBottles(data.message.msgBody);
+      setReLoad(false);
     }
+  };
 
-    const deleteBottle = async (_id) => {
-        if(_id === undefined){
-            return;
-        }else{
-            const data = await AuthService(`bottles?id=${_id}`, "delete");
-            setReLoad(true);
-        }
+  const deleteBottle = async (_id) => {
+    if (_id === undefined) {
+      return;
+    } else {
+      const data = await AuthService(`bottles?id=${_id}`, "delete");
+      if (data) {
+        setReLoad(true);
+      }
     }
+  };
 
-    useEffect(() => {
-        getBottle();
-    }, [reLoad]);
+  useEffect(() => {
+    if (reLoad) {
+      getBottle();
+    }
+  }, [reLoad]);
 
-    return(
-        <div className="whisky" >
-            <button className="newbottle" onClick={() => {setIsTrue(true)}} >New Bottle</button>
-            <Modal open={isTrue} onClose={() => setIsTrue(false)} reload={() => setReLoad(true)} title="New Bottle" />
-            { authenticated ? Object.entries(userBottles).map(([key, value], i) => {
-                return(
-                    <Bottle key={i} props={value} reload={setReLoad} deleteBottle={deleteBottle} ></Bottle>
-                )
-            })
-        :
+  return (
+    <div className="whisky">
+      <button
+        className="newbottle"
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        New Bottle
+      </button>
+      <Modal
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        reload={() => setReLoad(true)}
+        title="New Bottle"
+      />
+      {authenticated ? (
+        Object.entries(userBottles).map(([key, value], i) => {
+          return (
+            <Bottle
+              key={i}
+              props={value}
+              reload={() => setReLoad(true)}
+              deleteBottle={deleteBottle}
+            ></Bottle>
+          );
+        })
+      ) : (
         <div>loading...</div>
-        }
-        </div>
-    )
-}
+      )}
+    </div>
+  );
+};
